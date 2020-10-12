8242bbc1aeaf550877558ef1ab6e3a0e6b424f72
Kartik-Excellence <kartik@excellencetechnologies.info>
2020-10-12 11:39:43 +0530

    @@2

--- lib/request/postrequest.dart
+++ lib/request/postrequest.dart
@@ -1,16 +1,16 @@
 import 'dart:convert';
 
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:todos/model/post.dart';
 
 List<Post> postList = new List();
-Future<List<Post>> fetchPost() async {
-  final response = await get('https://jsonplaceholder.typicode.com/todos');
+Future<List<Post>> fetchData() async {
+  final response = await http.get('https://jsonplaceholder.typicode.com/todos');
   List<dynamic> values = List();
   values = json.decode(response.body);
   for (int i = 0; i < values.length; i++) {
     Map<String, dynamic> map = values[i];
     postList.add(Post.fromJson(map));
   }
-  print(postList[2].title);
+  return postList;
 }
--- lib/widget/homescreen.dart
+++ lib/widget/homescreen.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:todos/model/post.dart';
+import 'package:todos/request/postrequest.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
+
 class _HomeScreenState extends State<HomeScreen> {
   //bool isComplete = false;
   List<Post> getData;
-  // @override
-  // initState() {
-  //   super.initState();
-  //   fetchData().then((value) {
-  //     setState(() {
-  //       getData = value;
-  //       for (int i = 0; i < getData.length; i++) {
-  //         print(getData[i].title);
-  //       }
-  //     });
-  //   });
-  // }
+  @override
+  void initState() {
+    super.initState();
+    fetchData().then((value) {
+      setState(() {
+        getData = value;
+        print(getData.length);
+      });
+    });
+    print(getData.length)
+  }
+
   @override
   Widget build(BuildContext context) {
     return GridView.count(
