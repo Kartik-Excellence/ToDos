041b81a2ab6abe554385de2de0797cad7d5ba716
Kartik-Excellence <kartik@excellencetechnologies.info>
2020-10-15 17:05:39 +0530

    Api call resove

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -7,75 +7,40 @@ class Home extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.white,
-          centerTitle: true,
-          title: Text(
-            'ToDos',
-            style: TextStyle(
-                color: Colors.black, fontFamily: 'PlayFAir', fontSize: 20),
-          ),
-        ),
+            backgroundColor: Colors.red[200],
+            centerTitle: true,
+            title: Text(
+              'ToDos',
+              style: TextStyle(
+                  color: Colors.black, fontFamily: 'PlayFAir', fontSize: 25,),
+                  
+            ),
+            
+            toolbarHeight: 70,
+            toolbarOpacity: 1,
+            bottom: PreferredSize(
+              child: Text('Make Your ToDos Here',
+              style: TextStyle(fontFamily: 'PlayFAir',)
+              ,), preferredSize: Size.fromHeight(kToolbarHeight)),
+            leading: GestureDetector(
+                child: Icon(Icons.menu),
+                onTap: () {
+                  print('tap');
+                }),
+               // textTheme: TextTheme(headline1: Text),
+                actions: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(right: 20),
+                    child: Icon(Icons.add)
+                  ),
+                    Padding(padding: EdgeInsets.only(right: 16),
+                    child: Icon(Icons.delete),
+                    )
+                ],
+                ),
+              
         body: HomeScreen(),
       ),
-      
     );
   }
 }
-//   List<Post> getData;
-//   bool isLoading = false;
-//   Future loadData() async {
-//     await new Future.delayed(new Duration(seconds: 3));
-//   }
-
-//   @override
-//   void setState(fn) {
-//     super.setState(fn);
-//     print('SCrolling');
-//     isLoading = false;
-//   }
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   fetchPost().then((value) {
-  //     setState(() {
-  //       getData = value;
-  //     });
-  //   });
-  // }
-
-// @override
-// Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: Colors.white,
-//           centerTitle: true,
-//           title: Text(
-//             'ToDos',
-//             style: TextStyle(
-//                 color: Colors.black, fontFamily: 'PlayFAir', fontSize: 20),
-//           ),
-//         ),
-//         body: GridView.count(
-//           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-//           crossAxisSpacing: 5,
-//           mainAxisSpacing: 5,
-//           crossAxisCount: 2,
-//           children: List.generate(10, (index) {
-//             return ListTile(
-//               title: Text(getData[index].title),
-//               tileColor: getData[index].completed ? Colors.green : Colors.blue,
-//               contentPadding: EdgeInsets.all(5),
-//               subtitle: Text(
-//                 'Completed Status ${getData[index].completed.toString()}',
-//                 style: TextStyle(
-//                     color: Colors.yellow[300], fontFamily: 'PlayFAir'),
-//               ),
-//             );
-//           }),
-//         ),
-//       ),
-//     );
-//   }
-// }
--- lib/widget/homescreen.dart
+++ lib/widget/homescreen.dart
@@ -9,46 +9,62 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool isLoading;
-  List<Post> getData;
-  @override
-  void initState() {
-    super.initState();
-    fetchPost().then((value) {
-      setState(() {
-        getData = value;
-      });
+  bool isLoading = false;
+  List<Post> printData = new List();
+  List<Post> getData = new List();
+  int start, end;
+  Future loadData() async {
+    await new Future.delayed(new Duration(seconds: 3));
+    fetchPost().then((value) => {
+      for(int i =0;i<10;i++){
+        printData.add(value[i]),
+      }
     });
+    print('call after 10');
+    isLoading = false;
   }
 
   @override
   Widget build(BuildContext context) {
-    ScrollController scrollController = ScrollController();
-    scrollController.addListener(() {
-      if (scrollController.position.maxScrollExtent ==
-          scrollController.position.pixels) {
-        if (!isLoading) {
-          isLoading = !isLoading;
-          print("scrolling to nexty");
-        }
-      }
-    });
-    return GridView.count(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-      crossAxisSpacing: 5,
-      mainAxisSpacing: 5,
-      crossAxisCount: 2,
-      children: List.generate(getData.length, (index) {
-        return ListTile(
-          title: Text(getData[index].title),
-          tileColor: getData[index].completed ? Colors.green : Colors.blue,
-          contentPadding: EdgeInsets.all(5),
-          subtitle: Text(
-            'Completed Status ${getData[index].completed.toString()}',
-            style: TextStyle(color: Colors.yellow[300], fontFamily: 'PlayFAir'),
+    return Column(
+      children: <Widget>[
+        Expanded(
+            child: NotificationListener<ScrollNotification>(
+          onNotification: (ScrollNotification scrollNotification) {
+            if (!isLoading &&
+                scrollNotification.metrics.pixels ==
+                    scrollNotification.metrics.maxScrollExtent) {
+              loadData();
+              setState(() {
+                isLoading = true;
+              });
+            }
+            return false;
+          },
+
+          child: GridView.count(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            crossAxisSpacing: 5,
+            mainAxisSpacing: 5,
+            crossAxisCount: 2,
+            children: List.generate(printData.length, (index) {
+              return ListTile(
+                title: Text(printData[index].title),
+                tileColor:
+                    printData[index].completed ? Colors.green : Colors.blue,
+                contentPadding: EdgeInsets.all(5),
+                subtitle: Text(
+                  
+                  'Completed Status ${printData[index].completed.toString()}',
+                  style: TextStyle(
+                      color: Colors.yellow[300], fontFamily: 'PlayFAir'),
+                ),
+              );
+            }),
           ),
-        );
-      }),
+         
+        ))
+      ],
     );
   }
 }
